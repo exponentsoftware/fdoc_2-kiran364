4597c49399729f9c20d3a675623d93a30f99a858
kiran364 <kumawatkiran364@gmail.com>
2021-08-10 14:54:19 +0530

    javascript assignment

---
+++ contries.js
@@ -0,0 +1,18 @@
+const countries = [
+    'ALBANIA',
+    'BOLIVIA',
+    'CANADA',
+    'DENMARK',
+    'ETHIOPIA',
+    'FINLAND',
+    'GERMANY',
+    'HUNGARY',
+    'IRELAND',
+    'JAPAN',
+    'KENYA'
+  ]
+const count = countries.map((x) => {
+  return [x, x.length];
+})
+console.log(count);
+
